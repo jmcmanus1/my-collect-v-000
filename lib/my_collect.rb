a1a1ab80c87a9c13097da8dfe613057a1734d3d9
Julie  McManus <julieannesays@gmail.com>
2020-01-24 23:50:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 def my_collect(array)
+  i = 0
+  collection = []
+
+  while 0 < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
+
+my_collection(["Julie", "Tom", "Jim"]) {|name| "Hi #{name}."}
